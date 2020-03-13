9c88ac2e33526cc67276da4697fd1c85c4d7733a
Timofey <timaluk@icloud.com>
2020-03-13 12:24:35 +0300

    feat: realase task 5

--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -4,7 +4,34 @@
 
 // Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
-    return @"";
+    
+    NSMutableArray *arr = [[NSMutableArray alloc] initWithArray:@[]];
+    [s enumerateSubstringsInRange:(NSRange){0, [s length]} options: NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString * _Nullable substring, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {[arr addObject:substring];}];
+    
+    int chengeCount = 0;
+    long previouseMax = 0;
+    
+    for (long i = ([n intValue] / 2); i >= 0; i -= 1) {
+        
+        NSInteger firstElement = [[arr objectAtIndex: i] integerValue];
+        NSInteger secondElement = [[arr objectAtIndex: ([n intValue] - i - 1) ] integerValue];
+        if (firstElement != secondElement) {
+            long max = MAX(previouseMax,MAX(firstElement, secondElement));
+            previouseMax = max;
+            if (firstElement != max) {
+                chengeCount += 1;
+            }
+            if (secondElement != max) {
+                chengeCount += 1;
+            }
+            arr[i] = [NSString stringWithFormat:@"%ld", max];
+            arr[([n intValue] - i - 1)] = [NSString stringWithFormat:@"%ld", max];
+        }
+        
+    }
+    
+    
+    return  chengeCount == [k intValue] ? [arr componentsJoinedByString:@""] : @"-1";
 }
 
 @end
